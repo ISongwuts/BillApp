603ed4468d13c740b2a6415c7c97eca0ab7450e0
1tpp <panapat.dev@gmail.com>
2023-03-29 00:36:38 +0700

    bug: docx2pdf

--- billGen.py
+++ billGen.py
@@ -1,19 +1,17 @@
 from docxtpl import DocxTemplate
-from docx2pdf import convert
 from pdf2image import convert_from_path
+# from docx2pdf import convert
 import os
 import glob
 
 class DocumentGenerator:
     def render(self, name, addr, billNumber, date, taxid, total, productList):
-            import os
-            BASE_DIR = os.path.dirname(os.path.abspath(__file__))
             
             PrimaryDocument = DocxTemplate("template/PrimaryTemplate.docx")
-            PrimaryOutput = os.path.join(BASE_DIR, "generator/doc/generator_bill_primary.docx")
+            PrimaryOutput = os.path.join("generator/doc", "generator_bill_primary.docx")
 
             SecondaryDocument = DocxTemplate("template/SecondaryTemplate.docx")
-            SecondaryOutput = os.path.join(BASE_DIR, "generator/doc/generator_bill_secondary.docx")
+            SecondaryOutput = os.path.join("generator/doc", "generator_bill_secondary.docx")
             thai_month = ["ม.ค.", "ก.พ.", "มี.ค.", "เม.ย.", "พ.ค.", "มิ.ย.", "ก.ค.", "ส.ค.", "ก.ย.", "ต.ค.", "พ.ย.", "ธ.ค."]
             month, day, year = date.split("/")
             formatDateThai = f"{day} {thai_month[int(month)-1]} {year}"
@@ -39,20 +37,29 @@ class DocumentGenerator:
             SecondaryDocument.save(SecondaryOutput)
 
             if os.path.exists(PrimaryOutput) and os.path.exists(SecondaryOutput):
-                pdfCounter = len(glob.glob1("generator/pdf/primary/","*.pdf"))
-                pngCounter = len(glob.glob1("generator/png/primary/","*.png"))
-                convert(PrimaryOutput, f"generator/pdf/primary/converted_{pdfCounter+1}_primary.pdf")
-                convert(SecondaryOutput, f"generator/pdf/secondary/converted_{pdfCounter+1}_secondary.pdf")
+                pdfCounter = len(glob.glob1("generator/pdf/primary/", "*.pdf"))
+                pngCounter = len(glob.glob1("generator/png/primary/", "*.png"))
+                
+                primary_pdf_path = os.path.join("generator/pdf/primary", f"converted_{pdfCounter+1}_primary.pdf")
+                secondary_pdf_path = os.path.join("generator/pdf/secondary", f"converted_{pdfCounter+1}_secondary.pdf")
 
-                print(PrimaryOutput)
-                pagePrimary = convert_from_path(f"generator/pdf/primary/converted_{pdfCounter+1}_primary.pdf", dpi=95)
-                pageSecondary = convert_from_path(f"generator/pdf/secondary/converted_{pdfCounter+1}_secondary.pdf", dpi=95)
+                # TODO: docx2pdf is not implemented for macos apple silicon (ventuna)
+                # f = open(primary_pdf_path, "w")
+                # f.close()
+                # f2 = open(secondary_pdf_path, "w")
+                # f2.close()
+
+                # convert(PrimaryOutput, primary_pdf_path, keep_active=True)
+                # convert(SecondaryOutput, secondary_pdf_path, keep_active=True)
+
+
+                pagePrimary = convert_from_path(primary_pdf_path, dpi=95)
+                pageSecondary = convert_from_path(secondary_pdf_path, dpi=95)
+
                 for i, page in enumerate(pagePrimary):
-                    page.save(f"generator/png/primary/converted_{pngCounter+1}_primary.png", "PNG")
+                    page.save(os.path.join("generator/png/primary", f"converted_{pngCounter+1}_primary.png"), "PNG")
                 for i, page in enumerate(pageSecondary):
-                    page.save(f"generator/png/secondary/converted_{pngCounter+1}_secondary.png", "PNG")
+                    page.save(os.path.join("generator/png/secondary", f"converted_{pngCounter+1}_secondary.png"), "PNG")
 
                 return True
             return False
-        
-
